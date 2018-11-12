4b81584137d7c36ea80bfed0207d8123dbb9bff8
zbs881314 <34663445+zbs881314@users.noreply.github.com>
2018-11-12 10:33:52 -0500

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -10,8 +10,8 @@ rom keras.optimizers import Adam
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 # data pre-processing
-X_train = X_train.reshape(-1, 1, 28, 28)/255
-X_test = X_test.reshape(-1, 1, 28, 28)/255
+X_train = X_train.reshape(-1, 1, 28, 28)/255.
+X_test = X_test.reshape(-1, 1, 28, 28)/255.
 y_train = np_utils.to_categorical(y_train, num_classes=10)
 y_test = np_utils.to_categorical(y_test, num_classes=10)
 
