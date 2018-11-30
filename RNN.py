9b829929d9156e78eb7d301606f786e3ae04ffa7
bob <shibo.zhou14@gmail.com>
2018-11-29 21:23:15 -0500

    Update

--- RNN.py
+++ RNN.py
@@ -31,7 +31,7 @@ model = Sequential()
 # RNN cell
 # for batch_input_shape, if using tensorflow as the backend, we have to put None for the batch_size 
 # otherwise, model.evaluate() will get error.
-model.add(SimpleRNN(batch_input_shape=(None, Time_STEPS, INPUT_SIZE), output_dim=CELL_SIZE, unroll=True,))   #input_dim=INPUT_SIZE, input_length=TIME_STEPS
+model.add(SimpleRNN(batch_input_shape=(None, TIME_STEPS, INPUT_SIZE), output_dim=CELL_SIZE, unroll=True,))   #input_dim=INPUT_SIZE, input_length=TIME_STEPS
 
 # output layer
 model.add(Dense(OUTPUT_SIZE))
@@ -48,11 +48,11 @@ for step in range(4001):
     Y_batch = y_train[BATCH_INDEX: BATCH_INDEX+BATCH_SIZE, :]
     cost = model.train_on_batch(X_batch, Y_batch)
     BATCH_INDEX += BATCH_SIZE
-    BATCH_INDEX = 0 if BATCH_INDEX >= X_train.shape[0], else BATCH_INDEX
+    BATCH_INDEX = 0 if BATCH_INDEX >= X_train.shape[0] else BATCH_INDEX
     
     if step % 50 == 0:
         cost, accuracy = model.evaluate(X_test, y_test, batch_size=y_test.shape[0], verbose=False)
-        print('test cost:', test accuracy:', accuracy)
+        print('test cost:', cost, 'test accuracy:', accuracy)
 
 
 
